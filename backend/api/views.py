0560a937cc2a9c50955f9fe57959e08767899350
group117 <group117@elec377.queensu.ca>
2024-07-06 15:09:23 -0400

    routing works!!!

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,11 @@
+# views.py
+
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def test_page(request):
+    return HttpResponse('This is a test page to verify if the django setup works')
+
+def api_home(request):
+    return HttpResponse('API Landing page')
