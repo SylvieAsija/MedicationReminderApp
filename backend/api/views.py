a1d5c0b4a5ad1226412005f96ce049484ce4d12a
SylvieAsija <19aa10@queensu.ca>
2024-07-06 19:07:36 -0400

    style guide fix

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,9 +5,13 @@ from django.http import HttpResponse
 from django.conf import settings
 import os
 
+
 # Create your views here.
 def test_page(request):
-    return HttpResponse('This is a test page to verify if the django setup works')
+    return HttpResponse('This is a test page to verify if the \
+                        django setup works')
+
 
 def api_home(request):
-    return render(request, os.path.join(settings.FRONTEND_DIR, 'api_home.html'))
+    return render(request, os.path.join(settings.FRONTEND_DIR, \
+                  'api_home.html'))
