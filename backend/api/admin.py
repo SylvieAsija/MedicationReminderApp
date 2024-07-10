a2c6df03fe006055a23c5684bb2a190b9abbceb3
group117 <group117@elec377.queensu.ca>
2024-07-10 18:27:42 -0400

    managed migrations and db updates to accomodate for different authorization for users

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import MedicationInfo, MedicationLog
+from .models import Refills, SymptomLog
+from .models import Users
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('age', 'phone_number', 'birthday', 'blood_type', 'height', 'weight')}),
+    )
+
+admin.site.register(Users, UserAdmin)
+admin.site.register(MedicationInfo)
+admin.site.register(MedicationLog)
+admin.site.register(Refills)
+admin.site.register(SymptomLog)
+
