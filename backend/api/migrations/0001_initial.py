19d15e1fd2b62c0995b3bb4f09589e36dc4331c2
SylvieAsija <19aa10@queensu.ca>
2024-07-11 14:23:09 -0400

    style fix

--- backend/api/migrations/0001_initial.py
+++ backend/api/migrations/0001_initial.py
@@ -24,22 +24,22 @@ class Migration(migrations.Migration):
                                               verbose_name='password')),
                 ('last_login', models.DateTimeField(blank=True, 
                                                     null=True, 
-                                                    verbose_name=
-                                                    'last login')),
+                                                    verbose_name='last\
+                                                        login')),
                 ('is_superuser', models.BooleanField(default=False, 
                                                      help_text='Designates\
                 that this user has all permissions without explicitly\
                 assigning them.', 
                                                      verbose_name='superuser\
                                                          status')),
-                ('username', models.CharField(error_messages=
-                {'unique': 'A user with that username already exists.'}, 
-                help_text='Required. 150 characters or fewer. Letters,\
-                digits and @/./+/-/_ only.', 
+                ('username', models.CharField(error_messages={'unique': 
+                'A user with that username already exists.'}, 
+                                              help_text='Required. 150\
+                characters or fewer. Letters, digits and @/./+/-/_ only.',
                                                       max_length=150, 
                                                       unique=True, 
-                validators=[django.contrib.auth.validators.
-                            UnicodeUsernameValidator()], 
+                                                      validators=[django.
+                contrib.auth.validators.UnicodeUsernameValidator()], 
                                                       verbose_name=
                                                       'username')),
                 ('first_name', models.CharField(blank=True, 
