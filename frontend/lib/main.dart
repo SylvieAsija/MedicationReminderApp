e98c2c6b763b71156c40cbc5a759447e8b991ae2
Catarina Borges <theonlycat2015@gmail.com>
2024-07-12 17:03:12 -0700

    updated all files
    I tried running a simulation using the old files but couldn't. I noticed some files were missing from the default flutter application and others were not up-to-date. Since I couldn't find out exactly what was needed, and we hadn't started this part, I deleted all the files and recreated them using the 'flutter create [appname]' command in the terminal.

--- frontend/lib/main.dart
+++
@@ -1,9 +0,0 @@
-// start point for dart files
-// I hate workflow setup
-// This is pain
-// Every commit and test prolongs the pain
-// Is it too late to make everything in JS
-// Really hope it works this time
-// MEOW MROW
-// If this works I'm gonna be sad
-// I think I see the light
---
+++ frontend/med_track/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:med_track/home.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home()
+  ));
+}
