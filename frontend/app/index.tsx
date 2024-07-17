60c4abb6b13a5fa86130c1572351ca56944cf633
SylvieAsija <19aa10@queensu.ca>
2024-07-17 16:43:13 -0400

    style and bug fix

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,9 +1,9 @@
 // App.tsx
 import * as React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-//import HomeScreen from '../screens/HomeScreen';
-import LoginScreen from '../screens/LoginScreen';
-import Signup from '../screens/SignUp';
+import HomeScreen from '@/screens/HomeScreen';
+import LoginScreen from '@/screens/LoginScreen';
+import Signup from '@/screens/SignUp';
 
 type RootStackParamList = {
     Login: undefined;
@@ -18,7 +18,7 @@ const App: React.FC = () => {
         <Stack.Navigator initialRouteName='Login'>
             <Stack.Screen name='Login' component={LoginScreen} />
             <Stack.Screen name='Signup' component={Signup} />
-            {/* <Stack.Screen name='Home' component={HomeScreen} /> */}
+            <Stack.Screen name='Home' component={HomeScreen} />
         </Stack.Navigator>
     );
 };
