6eb687b87a760de0a1b88d8a398775b4c08a6598
SylvieAsija <19aa10@queensu.ca>
2024-07-18 12:58:03 -0400

    nicer looking login page

--- frontend/screens/LoginScreen.tsx
+++ frontend/screens/LoginScreen.tsx
@@ -1,10 +1,12 @@
 // LoginScreen.tsx
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import LoginButtonToggle from '@/components/loginSignupToggle';
+import { useFonts, Inter_500Medium, Inter_600SemiBold, Inter_700Bold } from '@expo-google-fonts/inter'
+import { PlusJakartaSans_500Medium } from '@expo-google-fonts/plus-jakarta-sans'
 
 type RootStackParamList = {
     Login: undefined;
@@ -15,6 +17,9 @@ type RootStackParamList = {
 type Props = NativeStackScreenProps<RootStackParamList, 'Login'>;
 
 const LoginScreen: React.FC<Props> = ({ navigation }) => {
+
+    let [fonts] = useFonts({Inter_500Medium, Inter_600SemiBold, Inter_700Bold, PlusJakartaSans_500Medium});
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [isLogin, setIsLogin] = useState(true);
@@ -50,33 +55,39 @@ const LoginScreen: React.FC<Props> = ({ navigation }) => {
             navigation.navigate('Signup');
     }
 
-    return (
-        <View style={styles.container}>
-                <Text style={styles.title}>Get Started Now</Text>
-                <Text style={styles.description}>Create an account or log in to use DoseUp</Text>
-                <View style={styles.buttonContainer}>
-                    <LoginButtonToggle isLogin={isLogin} setIsLogin={handleToggle} /> 
+    if (!fonts) 
+        return <View></View>
+    else {
+        return (
+            <View style={styles.container}>
+                    <Text style={styles.title}>Get Started Now</Text>
+                    <Text style={styles.description}>Create an account or log in to use DoseUp</Text>
+                    <View style={styles.buttonContainer}>
+                        <LoginButtonToggle isLogin={isLogin} setIsLogin={handleToggle} /> 
+                    </View>
+                <View>
+                    <TextInput 
+                        style={styles.input}
+                        placeholder='email'
+                        value={email}
+                        onChangeText={setEmail}
+                        keyboardType='email-address'
+                        autoCapitalize='none'
+                    />
+                    <TextInput 
+                        style={styles.input}
+                        placeholder='password'
+                        value={password}
+                        onChangeText={setPassword}
+                        secureTextEntry            
+                    />
+                    <TouchableOpacity style={styles.button} onPress={handleLogin} >
+                        <Text style={styles.buttonText}>Log In</Text>
+                    </TouchableOpacity>
                 </View>
-            <View>
-                <TextInput 
-                    style={styles.input}
-                    placeholder='email'
-                    value={email}
-                    onChangeText={setEmail}
-                    keyboardType='email-address'
-                    autoCapitalize='none'
-                />
-                <TextInput 
-                    style={styles.input}
-                    placeholder='password'
-                    value={password}
-                    onChangeText={setPassword}
-                    secureTextEntry            
-                />
-                <Button title='Login' onPress={handleLogin} />
             </View>
-        </View>
-    )
+        )
+    }
 }
 
 const styles = StyleSheet.create ({
@@ -87,15 +98,16 @@ const styles = StyleSheet.create ({
         backgroundColor: '#FFFFFF'
     },
     title: {
-        fontSize: 24,
-        marginBottom: 16,
+        fontSize: 32,
         textAlign: 'center',
         marginTop: 20,
-        fontFamily: 'Inter',
+        fontFamily: 'Inter_700Bold',
     },
     description: {
-        fontSize: 16,
+        fontSize: 12,
         textAlign: 'center',
+        fontFamily: 'Inter_500Medium',
+        marginBottom: 10,
     },
     buttonContainer: {
         flexDirection: 'row',
@@ -109,10 +121,26 @@ const styles = StyleSheet.create ({
         borderWidth: 1,
         marginBottom: 10,
         paddingHorizontal: 8,
+        borderRadius: 5
     },
     button: {
         alignSelf: 'center',
-        borderRadius: 6,
+        justifyContent: 'center',
+        borderRadius: 5,
+        backgroundColor: '#46D6CF',
+        width: '100%',
+        height: '20%'
+    },
+    buttonText: {
+        alignSelf: 'center',
+        fontFamily: 'Inter_600SemiBold',
+        fontSize: 14,
+        color: '#FFFFFF'
+    },
+    label: {
+        alignSelf: 'baseline',
+        fontFamily: 'PlusJakartaSans_500Medium',
+        fontSize: 12
     }
 });
 
