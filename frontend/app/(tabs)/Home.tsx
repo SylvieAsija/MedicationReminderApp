cb6de0612041228aa071fa78faeda0a05636f4d6
AnonymousKitty <theonlycat2015@gmail.com>
2024-07-27 00:10:04 -0400

    removed some unnecessary lines

--- frontend/app/(tabs)/Home.tsx
+++ frontend/app/(tabs)/Home.tsx
@@ -1,17 +1,15 @@
-import React, { useEffect, useState, useRef, useCallback, useMemo } from 'react';
-import { View, Text, StyleSheet, ScrollView, Image, useWindowDimensions, Button } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { View, Text, StyleSheet, Image, useWindowDimensions } from 'react-native';
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
-import { SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
-import { images, icons } from '@/constants';
+import { images } from '@/constants';
 import MedicationCard from '@/components/medicationCard';
 import BottomSheet, { BottomSheetScrollView } from '@gorhom/bottom-sheet';
-import { Gesture, GestureHandlerRootView } from 'react-native-gesture-handler';
+
 const Home: React.FC = () => {
   
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  const [boxHeight, setBoxHeight] = useState(400)
 
   useEffect(() => {
     const getName = async () => {
@@ -61,9 +59,14 @@ const Home: React.FC = () => {
         />
       </View>
 
-
-      {/* <ScrollView contentContainerStyle={{ width: useWindowDimensions().width }}> */}
-      <BottomSheet ref={bottomSheetRef} snapPoints={['50%', '90%']} handleStyle={{backgroundColor: '#46d6cf', borderTopLeftRadius: 50, borderTopRightRadius: 50}}>
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={['50%', '90%']}
+        handleStyle={{
+          backgroundColor: '#46d6cf',
+          borderTopLeftRadius: 50,
+          borderTopRightRadius: 50
+        }}>
         <BottomSheetScrollView
           style= {{
             backgroundColor: '#46d6cf',
@@ -73,66 +76,21 @@ const Home: React.FC = () => {
             alignItems: 'center'
           }}
         >
-            <MedicationCard
-              time="08:00 AM"
-              dose="2 Pills (10 mg)"
-              note="Take before eating"
-              name="Vitamin B12"
-            />
-            <MedicationCard
-              time="12:00 AM"
-              dose="1 Puff of inhaler"
-              note=""
-              name="Albuterol"
-            />
-            <MedicationCard
-              time="12:00 AM"
-              dose="1 Puff of inhaler"
-              note=""
-              name="Albuterol"
-            />
-            <MedicationCard
-              time="12:00 AM"
-              dose="1 Puff of inhaler"
-              note=""
-              name="Albuterol"
-            />
-            <MedicationCard
-              time="12:00 AM"
-              dose="1 Puff of inhaler"
-              note=""
-              name="Albuterol"
-            />
-            <MedicationCard
-              time="12:00 AM"
-              dose="1 Puff of inhaler"
-              note=""
-              name="Albuterol"
-            />
-            <MedicationCard
-              time="12:00 AM"
-              dose="1 Puff of inhaler"
-              note=""
-              name="Albuterol"
-            />
-            <MedicationCard
-              time="12:00 AM"
-              dose="1 Puff of inhaler"
-              note=""
-              name="Albuterol"
-            />
-            <MedicationCard
-              time="12:00 AM"
-              dose="1 Puff of inhaler"
-              note=""
-              name="Albuterol"
-            />
+          <MedicationCard
+            time="08:00 AM"
+            dose="2 Pills (10 mg)"
+            note="Take before eating"
+            name="Vitamin B12"
+          />
+          <MedicationCard
+            time="12:00 AM"
+            dose="1 Puff of inhaler"
+            note=""
+            name="Albuterol"
+          />
         </BottomSheetScrollView>
       </BottomSheet>
-
-      {/*  */}
-      {/* </ScrollView> */}
-      </View> 
+    </View> 
   );
 };
 
--- frontend/constants/icons.js
+++ frontend/constants/icons.js
@@ -1,9 +1,7 @@
 import pillLogo from '@/assets/icons/pill-logo.png'
 import home from '@/assets/icons/temp-home.png'
-import drag from '@/assets/icons/drag.png'
 
 export default {
     pillLogo,
     home,
-    drag,
 }
